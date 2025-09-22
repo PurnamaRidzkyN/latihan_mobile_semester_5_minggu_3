403d10f83eb20f4383880a82b1e510fcda8bab88
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2025-09-22 20:01:41 +0700

    Initialize project structure with essential files and configurations

---
+++ bin/latihan_mobile_semester_5_minggu_3.dart
@@ -0,0 +1,5 @@
+import 'package:latihan_mobile_semester_5_minggu_3/latihan_mobile_semester_5_minggu_3.dart' as latihan_mobile_semester_5_minggu_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${latihan_mobile_semester_5_minggu_3.calculate()}!');
+}
---
+++ test/latihan_mobile_semester_5_minggu_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:latihan_mobile_semester_5_minggu_3/latihan_mobile_semester_5_minggu_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
