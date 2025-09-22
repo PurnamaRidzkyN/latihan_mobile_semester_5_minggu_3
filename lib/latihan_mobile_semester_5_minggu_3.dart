82a849ccdf46b40160b96be89e9c9cf7ab3e358b
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2025-09-22 20:04:56 +0700

    Add function to display BMI calculation history

--- lib/latihan_mobile_semester_5_minggu_3.dart
+++ lib/latihan_mobile_semester_5_minggu_3.dart
@@ -1,3 +1,9 @@
-int calculate() {
-  return 6 * 7;
+void tampilkanRiwayat(List<Map<String, dynamic>> riwayat) {
+  print("=== Riwayat Perhitungan BMI ===");
+  for (var i = 0; i < riwayat.length; i++) {
+    var data = riwayat[i];
+    print(
+      "${i + 1}. Tinggi: ${data['tinggi']} cm, Berat: ${data['berat']} kg, BMI: ${data['bmi']}, Kategori: ${data['kategori']}"
+    );
+  }
 }
